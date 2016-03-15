48187d2fee2620b0879d37695594e159dbf6529c
XpaMoBHuk <eugene.nikiforoff.projects@gmail.com>
2016-03-16 00:34:33 +1100

    tests refactored

--- WimdioApiProxy.v2.Tests/ShadowDeviceTests.cs
+++ WimdioApiProxy.v2.Tests/ShadowDeviceTests.cs
@@ -12,23 +12,11 @@ namespace WimdioApiProxy.v2.Tests
     [TestClass()]
     public class ShadowDeviceTests : BaseTests
     {
-        protected List<Device> DevicesCreated = new List<Device>();
-
-        public new void Dispose()
-        {
-            DevicesCreated.ForEach(async d => await Client.DeleteDevice(d.Id));
-            base.Dispose();
-        }
-
         [TestMethod()]
-        public void ShadowCommands_Positive()
+        public void ShadowCommands_CRUD_Positive()
         {
+            // create
             Device device = null;
-            Func<Task> f = async () => device = await CreateDevice(Client, DevicesCreated);
-            f.ShouldNotThrow();
-            device.Should().NotBeNull();
-
-
             var rnd = new Random();
             var guid = Guid.NewGuid();
             IEnumerable<Command> commands = new List<Command>
@@ -50,35 +38,40 @@ namespace WimdioApiProxy.v2.Tests
                     DueDate = DateTime.Now.AddDays(7)
                 },
             };
-            f = async () => await Client.CreateDeviceCommands(device.Id, commands);
-            f.ShouldNotThrow();
+            Func<Task> asyncFunction = async () => 
+            {
+                device = await CreateDevice(Client);
+                await Client.CreateDeviceCommands(device.Id, commands);
+            };
+            asyncFunction.ShouldNotThrow();
+
+            // read list
+            var limit = 3;
+            asyncFunction = async () => commands = await Client.ReadDeviceCommands(device.DevKey, limit);
+            asyncFunction.ShouldNotThrow();
+            commands.Should().NotBeNullOrEmpty();
 
 
+            // aknowledge
             var commandStates = commands.Select(x => new CommandState { Id = x.Id }).ToList();
             commandStates.First().Status = 0;
             commandStates.Last().Error = 13;
-            f = async () => await Client.AcknowledgeDeviceCommands(device.DevKey, commandStates);
-            f.ShouldNotThrow();
+            asyncFunction = async () => await Client.AcknowledgeDeviceCommands(device.DevKey, commandStates);
+            asyncFunction.ShouldNotThrow();
 
 
-            var limit = 3;
-            f = async () => commands = await Client.ReadDeviceCommands(device.DevKey, limit);
-            f.ShouldNotThrow();
-            commands.Should().NotBeNull();
-            commands.Should().NotBeEmpty();
-
-
+            // delete
             commands.ToList().ForEach(c => 
             {
-                f = async () => await Client.DeleteDeviceCommands(device.Id, c.Id);
-                f.ShouldNotThrow();
+                asyncFunction = async () => await Client.DeleteDeviceCommands(device.Id, c.Id);
+                asyncFunction.ShouldNotThrow();
             });
 
 
-            f = async () => commands = await Client.ReadDeviceCommands(device.DevKey, limit);
-            f.ShouldNotThrow();
-            commands.Should().NotBeNull();
-            commands.Should().BeEmpty();
+            // read list
+            asyncFunction = async () => commands = await Client.ReadDeviceCommands(device.DevKey, limit);
+            asyncFunction.ShouldNotThrow();
+            commands.Should().BeNullOrEmpty();
         }
     }
 }
